fa26ac0dfc312bed7d85d55cc6885962f769837c
Lane <idomywant@hotmail.com>
2015-08-11 14:49:45 +0800

    commit by kyle

--- app/src/main/java/com/asexample/kylelin/asintentexample/LinearLayoutActivity.java
+++ app/src/main/java/com/asexample/kylelin/asintentexample/LinearLayoutActivity.java
@@ -1,5 +1,5 @@
 package com.asexample.kylelin.asintentexample;
-
+//Add a comment in new AddLayout
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
